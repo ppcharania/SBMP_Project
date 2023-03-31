28452b82d2c55f78d9e3d0121d31217cb929d2ce
ppcharania <ppcharania@gmail.com>
2023-03-31 10:45:38 +0530

    Sidenav update

--- src/Pages/Departments/SideNav.js
+++ src/Pages/Departments/SideNav.js
@@ -14,43 +14,43 @@ const SideNav = () => {
     <div style={{float:'left', width:'30%', padding:'1px'}} className='SideMenu'>
     <List sx={style} component="nav" aria-label="mailbox folders">
     <ListItem button>
-        <Link to='/civilengg'><ListItemText style={{color:'black'}} primary="Civil Enginnering"/></Link>
+        <Link to='/civilengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Civil Enginnering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/mechanicalengg'><ListItemText style={{color:'black'}} primary="Mechanical Enginnering"/></Link>
+        <Link to='/mechanicalengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Mechanical Enginnering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/electricalengg'><ListItemText style={{color:'black'}} primary="Electrical Engineering"/></Link>
+        <Link to='/electricalengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Electrical Engineering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/industrialelec'><ListItemText style={{color:'black'}} primary="Industrial Electronics"/></Link>
+        <Link to='/industrialelec' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Industrial Electronics"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/plasticengg'><ListItemText style={{color:'black'}} primary="Plastic Enginnering"/></Link>
+        <Link to='/plasticengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Plastic Enginnering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/chemicalengg'><ListItemText style={{color:'black'}} primary="Chemical Enginnering"/></Link>
+        <Link to='/chemicalengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Chemical Enginnering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/digitalelec'><ListItemText style={{color:'black'}} primary="Digital Electronics"/></Link>
+        <Link to='/digitalelec' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Digital Electronics"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/computerengg'><ListItemText style={{color:'black'}} primary="Computer Enginnering"/></Link>
+        <Link to='/computerengg' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Computer Enginnering"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/informationtech'><ListItemText style={{color:'black'}} primary="Information Technology"/></Link>
+        <Link to='/informationtech' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Information Technology"/></Link>
     </ListItem>
     <Divider />
     <ListItem button>
-        <Link to='/electronicstelecom'><ListItemText style={{color:'black'}} primary="Electronics and Telecommunication"/></Link>
+        <Link to='/electronicstelecom' style={{textDecoration:'none'}}><ListItemText style={{color:'black'}} primary="Electronics and Telecommunication"/></Link>
     </ListItem>
     <Divider />
     </List>
